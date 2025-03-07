95ad61e61db89e35795fcd329ec290524a0fc4ca
Parthiban <parthiban1082000@gmail.com>
2025-03-07 20:08:46 +0530

    Duplicate import removed

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,5 @@
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
-const User = require("../models/User");
 
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
